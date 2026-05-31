876ad5f03f0d9b9534ce60b69d54a0b7321a455b
casaraizrd <cordovamia53@gmail.com>
2026-05-30 19:43:17 -0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC0rYj7ZssPN7u6TSSA8ayXw2Ce2RlU8uQ",
-  authDomain: "crm-casa-rai.firebaseapp.com",
-  projectId: "crm-casa-rai",
-  storageBucket: "crm-casa-rai.firebasestorage.app",
-  messagingSenderId: "867390894417",
-  appId: "1:867390894417:web:75b856c9aa2fead71fe370"
-};
